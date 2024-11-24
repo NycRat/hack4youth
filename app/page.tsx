4da68842ee019e6dc0f37f05538848e35f156b1a
Eric Xiao <ericxiao0000@gmail.com>
2024-11-24 13:21:01 -0800

    feat: add mobile bottom navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import Navbar from "@/components/navbar";
-import { ThemeToggleButton } from "@/components/theme-toggle-button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <section className="thin">
-        <h1 className="text-6xl">L</h1>
-        <ThemeToggleButton />
-        <Navbar />
+        <h1 className="text-6xl">ProTrackr</h1>
       </section>
+
+      <Navbar />
     </main>
   );
 }
